d327033ff9df9ebf903ff8f67ba2017836f5df8c
Syspherion <manuel.jaehnig@googlemail.com>
2021-10-22 14:29:16 +0000

    ElapsedTime implemented

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,30 @@
-#include <string>
-
 #include "format.h"
 
+#include <iostream>
+#include <string>
+
 using std::string;
+using std::to_string;
+using namespace std;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  long timeLeft = seconds % 3600;
+  long hours = (seconds - timeLeft) / 3600;
+  cout << "hours:" <<hours<<"\n";
+
+  string hoursString = (hours < 10 ? "0" : "") + to_string(hours);
+
+  
+  long minutes = (timeLeft - timeLeft % 60) / 60;
+  timeLeft = timeLeft % 60;
+
+  string minutesString = (minutes < 10 ? "0" : "") + to_string(minutes);
+
+  string secondsString = (timeLeft < 10 ? "0" : "") + to_string(timeLeft);
+
+  return hoursString + ":" + minutesString + ":" + secondsString;
+}
